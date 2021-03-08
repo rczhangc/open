e5b0fd580833a83bc1790c35080a80a54cbe90e3
rczhangc <asdzrc@163.com>
2021-03-08 17:07:16 +0800

    IP地址扫描工具

--- open-common/src/main/java/org/open/boot/util/wol/LanIp.java
+++ open-common/src/main/java/org/open/boot/util/wol/LanIp.java
@@ -1,37 +1,27 @@
 package org.open.boot.util.wol;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 /**
  * @author barnak
  */
 public class LanIp {
 
-    public static void main(String[] args) throws UnknownHostException, ExecutionException, InterruptedException {
-        LanPing lanPing = new LanPing();
-        Thread thread = new Thread(lanPing);
-        thread.start();
-        thread.join();
-//        for (FutureTask<HostInfo> futureTask : lanPing.getPingList()) {
-//            System.out.println(futureTask.get().host +"->>>>"+ futureTask.get().reachable);
-//        }
-        for (String ip : lanPing.getUsedHostList()) {
-            System.out.println(ip +"->>>>"+ true);
-        }
-    }
+    private static ThreadPoolTaskExecutor threadPool;
 
+    /**
+     * 网段Ping执行
+     */
+    private static class LanPing implements Callable<LanPing> {
 
-    private static class LanPing implements Runnable {
-
-        private final List<FutureTask<HostInfo>> pingList;
+        private final List<Future<HostInfo>> pingList;
 
         /**
          * 局域网查询所有可能的 IP 地址的结果集
@@ -41,7 +31,7 @@ public class LanIp {
         /**
          * 局域网查询已经使用的 IP 地址的结果集
          */
-        private final List<String> usedHostList;
+        private final List<HostInfo> usedHostList;
 
 
         public LanPing() throws UnknownHostException {
@@ -62,21 +52,7 @@ public class LanIp {
             usedHostList = new ArrayList<>();
         }
 
-        @Override
-        public synchronized void run() {
-            try {
-                for (String host : hostList) {
-                    Callable<HostInfo> callable = new ExecutePing(host);
-                    FutureTask<HostInfo> futureTask = new FutureTask<>(callable);
-                    pingList.add(futureTask);
-                    new Thread(futureTask).start();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public List<FutureTask<HostInfo>> getPingList() {
+        public List<Future<HostInfo>> getPingList() {
             return pingList;
         }
 
@@ -84,21 +60,48 @@ public class LanIp {
             return hostList;
         }
 
-        public List<String> getUsedHostList() throws ExecutionException, InterruptedException {
+        public synchronized List<HostInfo> getUsedHostList() throws ExecutionException, InterruptedException {
             if (CollectionUtils.isEmpty(pingList)) {
                 return usedHostList;
             }
-            for (FutureTask<HostInfo> futureTask : pingList) {
+            List<FutureTask<HostInfo>> usedHostFutureList = new ArrayList<>();
+            for (Future<HostInfo> futureTask : pingList) {
                 HostInfo hostInfo = futureTask.get();
-                if (hostInfo.getReachable()) {
-                    usedHostList.add(hostInfo.getHost());
+                if (!hostInfo.getReachable()) {
+                    continue;
                 }
+                Callable<HostInfo> callable = new ExecuteHostName(hostInfo);
+                FutureTask<HostInfo> infoTask = new FutureTask<>(callable);
+                getThreadPool().submit(infoTask);
+                usedHostFutureList.add(infoTask);
             }
+
+            for (FutureTask<HostInfo> futureTask : usedHostFutureList) {
+                long startTime = System.currentTimeMillis();
+                usedHostList.add(futureTask.get());
+                long endTime = System.currentTimeMillis();
+                System.out.println("耗时："+ (endTime - startTime));
+            }
+
             return usedHostList;
+        }
+
+        @Override
+        public LanPing call() {
+            for (String host : getHostList()) {
+                Callable<HostInfo> callable = new ExecutePing(host);
+                System.out.println("扫描并添加host："+ host);
+                Future<HostInfo> submit = getThreadPool().submit(callable);
+                pingList.add(submit);
+                System.out.println("pingList Size："+ pingList.size());
+            }
+            return this;
         }
     }
 
-
+    /**
+     * 执行Ping命令
+     */
     private static class ExecutePing implements Callable<HostInfo> {
 
         /**
@@ -119,6 +122,53 @@ public class LanIp {
     }
 
     /**
+     * 执行获取主机名称
+     */
+    private static class ExecuteHostName implements Callable<HostInfo> {
+
+        private final HostInfo hostInfo;
+
+        public ExecuteHostName(HostInfo hostInfo) {
+            this.hostInfo = hostInfo;
+        }
+
+        @Override
+        public HostInfo call() {
+            InetAddress inetAddress = hostInfo.getInetAddress();
+            hostInfo.setHostName(inetAddress.getHostName());
+            hostInfo.setCanonicalHostName(inetAddress.getCanonicalHostName());
+            return hostInfo;
+        }
+    }
+
+    private static ThreadPoolTaskExecutor getThreadPool() {
+        if (null == threadPool) {
+            initThreadPool();
+        }
+        return threadPool;
+    }
+
+    private static void initThreadPool() {
+        // 核心线程池数量
+        int corePoolSize = 255;
+        // 最大线程池数量
+        int maximumPoolSize = 255 * 4;
+        // 线程最大空闲时间，对核心线程无效
+        int keepAliveTime = 0;
+        // 拒绝策略，所有配置资源都被占用完毕时抛出异常，分页缓存更新不会产生影响
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.AbortPolicy();
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        pool.setCorePoolSize(corePoolSize);
+        pool.setKeepAliveSeconds(keepAliveTime);
+        pool.setMaxPoolSize(maximumPoolSize);
+        pool.setQueueCapacity(255 * 4);
+        pool.setRejectedExecutionHandler(handler);
+        pool.setThreadNamePrefix("page-cache-pool");
+        pool.initialize();
+        threadPool = pool;
+    }
+
+    /**
      * 主机信息
      */
     private static class HostInfo {
@@ -126,17 +176,27 @@ public class LanIp {
         /**
          * 主机IP地址
          */
-        private String host;
+        private final String host;
 
         /**
          * 能否Ping通
          */
-        private boolean reachable;
+        private final boolean reachable;
+
+        /**
+         * 主机别名
+         */
+        private String hostName;
+
+        /**
+         * 主机名称
+         */
+        private String canonicalHostName;
 
         /**
          * 主机信息 {@link InetAddress}
          */
-        private InetAddress inetAddress;
+        private final InetAddress inetAddress;
 
         public HostInfo(String host, boolean reachable, InetAddress inetAddress) {
             this.host = host;
@@ -148,24 +208,28 @@ public class LanIp {
             return host;
         }
 
-        public void setHost(String host) {
-            this.host = host;
-        }
-
         public boolean getReachable() {
             return reachable;
         }
 
-        public void setReachable(boolean reachable) {
-            this.reachable = reachable;
+        public String getHostName() {
+            return hostName;
+        }
+
+        public void setHostName(String hostName) {
+            this.hostName = hostName;
+        }
+
+        public String getCanonicalHostName() {
+            return canonicalHostName;
+        }
+
+        public void setCanonicalHostName(String canonicalHostName) {
+            this.canonicalHostName = canonicalHostName;
         }
 
         public InetAddress getInetAddress() {
             return inetAddress;
-        }
-
-        public void setInetAddress(InetAddress inetAddress) {
-            this.inetAddress = inetAddress;
         }
     }
 
@@ -185,5 +249,20 @@ public class LanIp {
          * 本地IP地址数量
          */
         int LOCAL_SIZE = 255;
+    }
+
+    public static void main(String[] args) throws UnknownHostException, ExecutionException, InterruptedException {
+        Future<LanPing> submit = getThreadPool().submit(new LanPing());
+        LanPing lanPing = submit.get();
+        System.out.println("pingList Size："+ lanPing.getPingList().size());
+        for (Future<HostInfo> futureTask : lanPing.getPingList()) {
+            System.out.println(futureTask.get().getHost() +"->>>>"+ futureTask.get().getReachable());
+        }
+        List<HostInfo> usedHostList = lanPing.getUsedHostList();
+        System.out.println("usedHostList Size："+ usedHostList.size());
+        for (HostInfo hostInfo : usedHostList) {
+            System.out.println(hostInfo.getHost() +"->>>>"+ hostInfo.getReachable() +"->>>>"+ hostInfo.getHostName() +"->>>>"+ hostInfo.getCanonicalHostName());
+        }
+        getThreadPool().shutdown();
     }
 }
